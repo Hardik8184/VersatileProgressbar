03ab24d13dc2a83bb6a1d29ee63941df444f9607
hdungrani@gotechark.com <p@ssword8184>
2019-07-23 14:27:01 +0530

    master: enhancements

--- app/src/androidTest/java/com/hardik/progressbar/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hardik/progressbar/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.aseem.multiprogressbar", appContext.getPackageName());
+        assertEquals(" com.hardik.progressbar", appContext.getPackageName());
     }
 }
